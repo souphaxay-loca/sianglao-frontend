b7c374c0ecccbb227a1aa4200328ed12010df70c
souphaxay-loca <souphaxay@loca.la>
2025-06-01 02:04:21 +0700

    init project with tailwind

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <!-- <h1 class="text-3xl font-bold underline text-blue-300">Hello World</h1>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline text-blue-300">Hello World</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
